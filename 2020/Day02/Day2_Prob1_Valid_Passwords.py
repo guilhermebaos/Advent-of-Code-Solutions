81e2a3e226f83bbb8847ab18ebb4faa55582031f
Guilherme Silva <guilhermebaos@gmail.com>
2020-12-02 22:27:14 +0000

    2020 Day 2

---
+++ 2020/Day02/Day2_Prob1_Valid_Passwords.py
@@ -0,0 +1,19 @@
+import re
+
+# Puzzle_input
+with open('Day2_Input.txt', 'r') as puzzle_input:
+    passwords = puzzle_input.read().split('\n')
+
+# Separate the input
+processed_passwords = []
+for pass_list in passwords:
+    processed_passwords += [re.split(r'[- :]', pass_list)]
+
+# See if passwords are valid
+valid = 0
+for pass_list in processed_passwords:
+    password = pass_list[4]
+    if int(pass_list[0]) <= password.count(pass_list[2]) <= int(pass_list[1]):
+        valid += 1
+
+print(valid)
---
+++ 2020/Day02/Day2_Prob2_Different_Valid_Passwords.py
@@ -0,0 +1,20 @@
+import re
+
+# Puzzle_input
+with open('Day2_Input.txt', 'r') as puzzle_input:
+    passwords = puzzle_input.read().split('\n')
+
+# Separate the input
+processed_passwords = []
+for pass_list in passwords:
+    processed_passwords += [re.split(r'[- :]', pass_list)]
+
+# See if passwords are valid
+valid = 0
+for pass_list in processed_passwords:
+    password = pass_list[4]
+    letter = pass_list[2]
+    if (password[int(pass_list[0]) - 1] == letter) != (password[int(pass_list[1]) - 1] == letter):    # != is an XOR operator
+        valid += 1
+
+print(valid)
