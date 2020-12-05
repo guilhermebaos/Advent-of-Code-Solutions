5cf3f8492cf26220161b787c3cd1c47044d90dba
Guilherme Silva <guilhermebaos@gmail.com>
2020-12-05 13:17:46 +0000

    2020 Day05
    + Correct Invalid.txt from Day04

---
+++ 2020/Day05/Day05_Prob1_Finding_Max_Seat.py
@@ -0,0 +1,29 @@
+# Puzzle Input
+with open('Day05_Input.txt') as puzzle_input:
+    seats = puzzle_input.read().split('\n')
+
+
+# Converts a list containing a binary number to a decimal number
+def bin_to_decimal(bin_num):
+    dec_num = 0
+    bin_len = len(bin_num)
+    for pos, bit in enumerate(bin_num):
+        if bit == 1:
+            dec_num += 2 ** (bin_len - pos - 1)
+    return dec_num
+
+
+# Calculates all the seats IDs
+seat_id = []
+for s in seats:
+    seat_position = [[], []]
+    # Convert from letters to binary
+    for letter in s[:-3]:
+        seat_position[0] += [0] if letter == 'F' else [1]
+    for letter in s[-3:]:
+        seat_position[1] += [0] if letter == 'L' else [1]
+
+    # Convert from binary to seat ID
+    seat_id += [bin_to_decimal(seat_position[0]) * 8 + bin_to_decimal(seat_position[1])]
+
+print(max(seat_id))
