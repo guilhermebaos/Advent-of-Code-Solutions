1cbb3051fb21d3aa089781dff68bcb5f28bcbbe3
Guilherme Silva <guilhermebaos@gmail.com>
2020-12-15 14:57:46 +0000

    2020 Day15

---
+++ 2020/Day15/Day15_Prob2_30000000th_Number_Spoken.py
@@ -0,0 +1,31 @@
+# Puzzle Input
+with open('Day15_Input.txt') as puzzle_input:
+    starting = puzzle_input.read().split('\n')
+
+# Separate the integers (convert from CSV to python list)
+for index, number_list in enumerate(starting):
+    starting[index] = number_list.split(',')
+
+# Parameters
+starting = list(map(int, starting[0]))  # Allows for easy change between the test cases
+len_starting = len(starting)            # Length of the starting list
+
+# Calculate the first turns
+memory = dict()                                     # Memory of the previous turns
+for last_turn in range(1, len_starting):            # For every last turn:
+    memory[starting[last_turn - 1]] = last_turn         # Add to the memory when each value was spoken last
+
+# Calculate the other turns
+last_number = starting[-1]                          # Select the last number spoken
+for last_turn in range(len_starting, 30000000):     # For every last turn:
+    try:                                                # See if the number was spoken before
+        age = last_turn - memory[last_number]               # If it was, register the age of the number
+    except KeyError:                                    # If it wasn't:
+        age = 0                                             # The age is 0
+    memory[last_number] = last_turn                 # Save to memory the turn when the last number was spoken
+    last_number = age                               # Update the last number
+    if last_turn % 1000000 == 0:                    # See progress
+        print(last_turn)                                # The program takes about 30 seconds
+
+# Show the result
+print(last_number)
