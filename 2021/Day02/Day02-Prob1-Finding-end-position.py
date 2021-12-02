45775f3bd66166105c7dcee509ae0e6c7a892883
Guilherme Silva <guilhermebaos@gmail.com>
2021-12-02 07:53:30 +0000

    Day 2 AoC 2021

---
+++ 2021/Day02/Day02-Prob1-Finding-end-position.py
@@ -0,0 +1,37 @@
+# Puzzle Input ----------
+with open('Day02-Input.txt', 'r') as file:
+    puzzle = file.read().split('\n')
+
+with open('Day02-Test01.txt', 'r') as file:
+    test01 = file.read().split('\n')
+
+
+# Main Code ----------
+
+# Split the command and number into a tuple and convert to the right data type
+def split_and_convert(commands):
+    new_commands = []
+    for c in commands:
+        c = c.split()
+        new_commands += [(c[0], int(c[1]))]
+    return new_commands
+
+
+# Find where the sumbarine goes
+def find_position(commands):
+    commands = split_and_convert(commands)
+
+    hor, dep = 0, 0
+    for c in commands:
+        if c[0] == 'forward':
+            hor += c[1]
+        elif c[0] == 'down':
+            dep += c[1]
+        else:
+            dep -= c[1]
+    return hor * dep
+
+
+# Tests and Solution ----------
+print(find_position(test01))
+print(find_position(puzzle))
