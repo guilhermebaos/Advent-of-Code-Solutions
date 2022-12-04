1e3559ac6bc6ae08ad0b948d376dac5c6730f6e4
Guilherme Silva <guilhermebaos@gmail.com>
2022-12-04 09:35:14 +0000

    Day 04 AoC 2022

--- 2022/Day4/Day4-Prob1.py
+++ 2022/Day4/Day4-Prob1.py
@@ -1,4 +1,5 @@
 # Solution for Problem 1 Day 4 of AoC 2022!
+import re
 
 # Puzzle Input ----------
 with open('Day4-Input.txt', 'r') as file:
@@ -10,7 +11,19 @@ with open('Day4-Test01.txt', 'r') as file:
 
 # Code ------------------
 def solution_day4_prob1(puzzle_in: list):
-    return
+    total = 0
+    for item in puzzle_in:
+        # Get range values
+        start1, end1, start2, end2 = list(map(int, re.split("[,-]", item)))
+
+        # See if 1 is inside 2
+        if start2 <= start1 and end1 <= end2:
+            total += 1
+
+        # See if 2 is inside 1
+        elif start1 <= start2 and end2 <= end1:
+            total += 1
+    return total
 
 
 # Tests and Solution ---
@@ -18,3 +31,5 @@ print("Tests:")
 print(solution_day4_prob1(test01))
 print("\nSolution:")
 print(solution_day4_prob1(puzzle))
+
+# 522 is too high
--- 2022/Day4/Day4-Prob2.py
+++ 2022/Day4/Day4-Prob2.py
@@ -1,4 +1,5 @@
 # Solution for Problem 2 Day 4 of AoC 2022!
+import re
 
 # Puzzle Input ----------
 with open('Day4-Input.txt', 'r') as file:
@@ -10,7 +11,17 @@ with open('Day4-Test01.txt', 'r') as file:
 
 # Code ------------------
 def solution_day4_prob2(puzzle_in: list):
-    return
+    total = 0
+    for item in puzzle_in:
+        # Get range values
+        start1, end1, start2, end2 = list(map(int, re.split("[,-]", item)))
+
+        # See if there is overlapping
+        if start2 <= end1 <= end2:
+            total += 1
+        elif start1 <= end2 <= end1:
+            total += 1
+    return total
 
 
 # Tests and Solution ---
