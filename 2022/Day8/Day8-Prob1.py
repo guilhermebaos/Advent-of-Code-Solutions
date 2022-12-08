4b5505b13ee1bf68902d2cf44bc67212f1af6343
Guilherme Silva <guilhermebaos@gmail.com>
2022-12-08 16:51:39 +0000

    Day 08 AoC 2022

--- 2022/Day8/Day8-Prob1.py
+++ 2022/Day8/Day8-Prob1.py
@@ -9,8 +9,60 @@ with open('Day8-Test01.txt', 'r') as file:
 
 
 # Code ------------------
+
+# Check the visibility within a line
+def check_visibility(x: int, y: int, mode: str, line: list, visible: list) -> list:
+    for i in range(2):
+
+        # Reverse direction
+        if i == 1:
+            line = line[::-1]
+
+        # Get visibility
+        max_height = -1
+        for z, tree in enumerate(line):
+
+            # If we can see the tree...
+            if tree > max_height:
+
+                # ... add it to total
+                max_height = tree
+
+                # ... and mark it as visible on the right row or column
+                if mode == "row":
+                    visible[y][z if i == 0 else -(z+1)] = True
+                elif mode == "col":
+                    visible[z if i == 0 else -(z+1)][x] = True
+
+    return visible
+
+
 def solution_day8_prob1(puzzle_in: list):
-    return
+    # Parse the input
+    trees = list(map(list, puzzle_in))
+    for index, item in enumerate(trees):
+        trees[index] = list(map(int, item))
+    x_max, y_max = len(trees[0]), len(trees)
+
+    # Visible trees
+    visible = [[0 for _ in range(x_max)] for _ in range(y_max)]
+
+    # Get the columns and check visibility
+    cols = [[] for _ in range(x_max)]
+    for y, row in enumerate(trees):
+        visible = check_visibility(0, y, "row", row, visible)
+        for x, tree in enumerate(row):
+            cols[x] += [tree]
+    for x, col in enumerate(cols):
+        visible = check_visibility(x, 0, "col", col, visible)
+
+    # Total number of visible trees
+    total = 0
+    for row in visible:
+        for item in row:
+            total += item
+
+    return total
 
 
 # Tests and Solution ---
--- 2022/Day8/Day8-Prob2.py
+++ 2022/Day8/Day8-Prob2.py
@@ -9,8 +9,62 @@ with open('Day8-Test01.txt', 'r') as file:
 
 
 # Code ------------------
+
+# Calculate scenis score
+def scenic_score(views: tuple) -> int:
+    total = 1
+    for item in views:
+        total *= item
+    return total
+
+
+# Get the view from a specific tree
+def check_view(x: int, y: int, h: int, x_max: int, y_max: int, trees: list) -> tuple:
+    view = [0, 0, 0, 0]
+    for x_check in range(x - 1, -1, -1):
+        view[0] += 1
+        if trees[y][x_check] >= h:
+            break
+
+    for x_check in range(x + 1, x_max):
+        view[1] += 1
+        if trees[y][x_check] >= h:
+            break
+
+    for y_check in range(y - 1, -1, -1):
+        view[2] += 1
+        if trees[y_check][x] >= h:
+            break
+
+    for y_check in range(y + 1, y_max):
+        view[3] += 1
+        if trees[y_check][x] >= h:
+            break
+
+    return tuple(view)
+
+
 def solution_day8_prob2(puzzle_in: list):
-    return
+    # Parse the input
+    trees = list(map(list, puzzle_in))
+    for index, item in enumerate(trees):
+        trees[index] = list(map(int, item))
+    x_max, y_max = len(trees[0]), len(trees)
+
+    # Get the columns
+    cols = [[] for _ in range(x_max)]
+    for y, row in enumerate(trees):
+        for x, tree in enumerate(row):
+            cols[x] += [tree]
+
+    # Calculate maximum score
+    scores = []
+    for y, row in enumerate(trees):
+        for x, tree in enumerate(row):
+            view = check_view(x, y, tree, x_max, y_max, trees)
+            scores += [scenic_score(view)]
+
+    return max(scores)
 
 
 # Tests and Solution ---
